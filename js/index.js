d5e536c4987ae2e70728c20c15d9b68a1a4e91e9
Vitor Domingos <vm486092@gmail.com>
2023-10-19 22:43:58 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,43 @@
+
+// Função de soma com return
+
+function somaEntreCincoNumeros(firstnumber,secondnumber,thirdnumber,fornumber,fivenumber){
+
+return firstnumber + secondnumber + thirdnumber + fornumber + fivenumber
+}
+
+const valorDaSomaEntreCincoNumeros = somaEntreCincoNumeros(68,95,83,40,72)
+
+console.log("A soma dos cinco números é:" + valorDaSomaEntreCincoNumeros)
+
+// Função de divisão com return
+
+function divisaoEntreCincoNumeros(firstnumber,secondnumber,thirdnumber,fournumber,fivenumber){
+
+return firstnumber / secondnumber / thirdnumber / fournumber / fivenumber
+}
+
+const valorDaDivisaoEntreCincoNumeros = divisaoEntreCincoNumeros(180,68,110,28,40)
+
+console.log("A divisão dos cinco números é:" + valorDaDivisaoEntreCincoNumeros)
+
+// Função de multiplicação com return
+
+function multiplicacaoEntreCincoNumeros(firstnumber,secondnumber,thirdnumber,fournumber,fivenumber){
+
+return firstnumber * secondnumber * thirdnumber * fournumber * fivenumber
+}
+
+const resultadoDaMultiplicacaoEntreCincoNumeros = multiplicacaoEntreCincoNumeros(75,19,10,84,7)
+
+console.log("A multiplicação dos cinco números é:" + resultadoDaMultiplicacaoEntreCincoNumeros)
+
+// Função de subtração com return
+
+function subtracaoEntreCincoNumeros(firstnumber,secondnumber,thirdnumber,fournumber,fivenumber){
+return firstnumber - secondnumber - thirdnumber - fournumber - fivenumber
+}
+
+const resultadoDaSubtracaoEntreCincoNumeros = subtracaoEntreCincoNumeros(62,5,7,2,8)
+
+console.log("A subtração dos cinco números é:" + resultadoDaSubtracaoEntreCincoNumeros)
